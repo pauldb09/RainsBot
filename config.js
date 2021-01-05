fa074aca3b5eee3e12bc6777659a33b3a7b8b866
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-01-05 17:27:14 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    support: {
+        id: "787006944974995476",
+        logs: "787070110832918608"
+    },
+    prefix: "?",
+    defaultsSettings: {
+        lang: "fr",
+        welcomeMessage: "Bienvenue {user} sur le serveur **{guildName}** !",
+        goodbyeMessage: "Aurevoir {user}, nous espérons te revoir bientôt !"
+    },
+    embed: {
+        color: "#4696d1",
+        footer: "RainsBot - Open Source"
+    },
+    owner: {
+        id: "383667743037194240",
+        name: "COCO150#0001"
+    },
+    status: [
+        {
+            name: "{serversCount} serveurs et {usersCount} utilisateurs",
+            type: "COMPETING"
+        },
+        {
+            name: "@RainsBot pour avoir de l'aide!",
+            type: "PLAYING"
+        }
+    ]
+}
