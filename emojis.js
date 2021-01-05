7837d76ea9f31d499d68d4bac3bcc20bd70c401d
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-01-05 17:26:37 +0100

    Add files via upload

---
+++ emojis.js
@@ -0,0 +1,21 @@
+module.exports = {
+    chargement: "<a:chargement:781955727580594188>",
+    facepalm: "<:facepalm:782305364490190858>",
+    online: "<:online:783032922457636904>",
+    dnd: "<:dnd:783032948399276134>",
+    idle: "<:idle:783032969219932191>",
+    offline: "<:offline:783032991407669279>",
+    textChannel: "<:textChannel:784752237301334046>",
+    voiceChannel: "<:voiceChannel:784752271434973186>",
+    boost: "<:boost:784753401867141120>",
+    partner: "<:partner:784756429293879336>",
+    categories: {
+        Administration: "🗃️",
+        Config: "🛠️",
+        General: "🌐",
+        Giveaways: "🎉",
+        Moderation: "⚒️",
+        Owner: "👑",
+        Protection: "🛡️"
+    }
+}
