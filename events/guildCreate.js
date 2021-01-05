5b7b4940b7b02cdc4f5a4c991756bb895e88d424
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-01-05 18:16:59 +0100

    🐛 bugfix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,6 +5,6 @@ module.exports = async (client, guild) => {
         .setColor('GREEN')
         .setAuthor(guild.name, guild.iconURL({ dynamic: true }))
         .setDescription(`J'ai rejoint le serveur ${guild.name} ! Je suis maintenant dans **` + client.guilds.cache.size + `** serveurs !`)
-        setFooter(client.config.embed.footer, client.user.displayAvatarURL())
+        .setFooter(client.config.embed.footer, client.user.displayAvatarURL())
     client.channels.cache.get(client.config.support.logs).send(newGuildEmbed);
 }
